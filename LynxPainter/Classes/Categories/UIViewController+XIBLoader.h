0b44526f5f10191e142a8d8b8b68f444071df39d
Dmitriy Shamaev <Dmitriy.Shamaev@dataart.com>
2012-11-06 01:35:48 +0400

    -category for loading views from xib

---
+++ LynxPainter/Classes/Categories/UIViewController+XIBLoader.h
@@ -0,0 +1,13 @@
+//
+//  UIViewController+XIBLoader.h
+//  LynxPainter
+//
+//  Created by Администратор on 11/6/12.
+//  Copyright (c) 2012 DmitriyShamaev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIViewController (XIBLoader)
+
+@end
---
+++ LynxPainter/Classes/Categories/UIViewController+XIBLoader.m
@@ -0,0 +1,13 @@
+//
+//  UIViewController+XIBLoader.m
+//  LynxPainter
+//
+//  Created by Администратор on 11/6/12.
+//  Copyright (c) 2012 DmitriyShamaev. All rights reserved.
+//
+
+#import "UIViewController+XIBLoader.h"
+
+@implementation UIViewController (XIBLoader)
+
+@end
