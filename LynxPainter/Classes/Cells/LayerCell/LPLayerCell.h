981e8c0943ad3539b9d45cb9ed9674c6a6d0ae9b
Dmitriy Shamaev <Dmitriy.Shamaev@dataart.com>
2012-11-05 13:36:54 +0400

    -layer cell layout for layers manager

---
+++ LynxPainter/Classes/Cells/LayerCell/LPLayerCell.h
@@ -0,0 +1,13 @@
+//
+//  LPLayerCell.h
+//  LynxPainter
+//
+//  Created by Администратор on 11/5/12.
+//  Copyright (c) 2012 DmitriyShamaev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LPLayerCell : UITableViewCell
+
+@end
---
+++ LynxPainter/Classes/Cells/LayerCell/LPLayerCell.m
@@ -0,0 +1,29 @@
+//
+//  LPLayerCell.m
+//  LynxPainter
+//
+//  Created by Администратор on 11/5/12.
+//  Copyright (c) 2012 DmitriyShamaev. All rights reserved.
+//
+
+#import "LPLayerCell.h"
+
+@implementation LPLayerCell
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
