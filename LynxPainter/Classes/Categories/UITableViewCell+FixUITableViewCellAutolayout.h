fe0b90150924d6cda4754d40e4ede33f8e17e161
Dmitriy Shamaev <Dmitriy.Shamaev@dataart.com>
2012-11-07 11:45:26 +0400

    -fix for uitableviewcell

---
+++ LynxPainter/Classes/Categories/UITableViewCell+FixUITableViewCellAutolayout.h
@@ -0,0 +1,13 @@
+//
+//  UITableViewCell+FixUITableViewCellAutolayout.h
+//  LynxPainter
+//
+//  Created by Администратор on 11/7/12.
+//  Copyright (c) 2012 DmitriyShamaev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UITableViewCell (FixUITableViewCellAutolayout)
+
+@end
---
+++ LynxPainter/Classes/Categories/UITableViewCell+FixUITableViewCellAutolayout.m
@@ -0,0 +1,13 @@
+//
+//  UITableViewCell+FixUITableViewCellAutolayout.m
+//  LynxPainter
+//
+//  Created by Администратор on 11/7/12.
+//  Copyright (c) 2012 DmitriyShamaev. All rights reserved.
+//
+
+#import "UITableViewCell+FixUITableViewCellAutolayout.h"
+
+@implementation UITableViewCell (FixUITableViewCellAutolayout)
+
+@end
