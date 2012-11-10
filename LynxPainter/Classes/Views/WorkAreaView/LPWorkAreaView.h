95d7188ab4d98e2936fa2aeb1a22d3e94f44f42c
Dmitriy Shamaev <Dmitriy.Shamaev@dataart.com>
2012-11-10 18:46:28 +0400

    -workarea view

---
+++ LynxPainter/Classes/Views/WorkAreaView/LPWorkAreaView.h
@@ -0,0 +1,13 @@
+//
+//  LPWorkAreaView.h
+//  LynxPainter
+//
+//  Created by DmitriyShamaev on 11/10/12.
+//  Copyright (c) 2012 DmitriyShamaev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LPWorkAreaView : UIView
+
+@end
---
+++ LynxPainter/Classes/Views/WorkAreaView/LPWorkAreaView.m
@@ -0,0 +1,31 @@
+//
+//  LPWorkAreaView.m
+//  LynxPainter
+//
+//  Created by DmitriyShamaev on 11/10/12.
+//  Copyright (c) 2012 DmitriyShamaev. All rights reserved.
+//
+
+#import "LPWorkAreaView.h"
+
+@implementation LPWorkAreaView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
