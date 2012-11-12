b12788081f9fc6558de4a8b9d2cb9d98e41ade29
Dmitriy Shamaev <Dmitriy.Shamaev@dataart.com>
2012-11-12 18:51:00 +0200

    -closedprojectdialog init

---
+++ LynxPainter/Classes/ViewControllers/CloseProjectDialog/LPCloseProjectDialogViewController.h
@@ -0,0 +1,13 @@
+//
+//  LPCloseProjectDialogViewController.h
+//  LynxPainter
+//
+//  Created by DmitriyShamaev on 12.11.12.
+//  Copyright (c) 2012 DmitriyShamaev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LPCloseProjectDialogViewController : UIViewController
+
+@end
---
+++ LynxPainter/Classes/ViewControllers/CloseProjectDialog/LPCloseProjectDialogViewController.m
@@ -0,0 +1,38 @@
+//
+//  LPCloseProjectDialogViewController.m
+//  LynxPainter
+//
+//  Created by DmitriyShamaev on 12.11.12.
+//  Copyright (c) 2012 DmitriyShamaev. All rights reserved.
+//
+
+#import "LPCloseProjectDialogViewController.h"
+
+@interface LPCloseProjectDialogViewController ()
+
+@end
+
+@implementation LPCloseProjectDialogViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
