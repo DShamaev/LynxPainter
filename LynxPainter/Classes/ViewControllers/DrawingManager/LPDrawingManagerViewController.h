e1a37fd56bee6785b59f60e88213570b43b0acdf
Dmitriy Shamaev <Dmitriy.Shamaev@dataart.com>
2012-11-13 02:00:28 +0400

    -drawingmanager init

---
+++ LynxPainter/Classes/ViewControllers/DrawingManager/LPDrawingManagerViewController.h
@@ -0,0 +1,13 @@
+//
+//  LPDrawingManagerViewController.h
+//  LynxPainter
+//
+//  Created by DmitriyShamaev on 11/13/12.
+//  Copyright (c) 2012 DmitriyShamaev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LPDrawingManagerViewController : UIViewController
+
+@end
