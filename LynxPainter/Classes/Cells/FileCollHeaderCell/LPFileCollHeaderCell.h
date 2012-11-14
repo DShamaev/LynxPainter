6d23971a56655f9b2b0d0c469ecd1ce25280ec08
Dmitriy Shamaev <Dmitriy.Shamaev@dataart.com>
2012-11-14 11:46:13 +0400

    -file section cells init

---
+++ LynxPainter/Classes/Cells/FileCollHeaderCell/LPFileCollHeaderCell.h
@@ -0,0 +1,13 @@
+//
+//  LPFileCollHeaderCell.h
+//  LynxPainter
+//
+//  Created by DmitriyShamaev on 11/14/12.
+//  Copyright (c) 2012 DmitriyShamaev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LPFileCollHeaderCell : UICollectionReusableView
+
+@end
---
+++ LynxPainter/Classes/Cells/FileCollHeaderCell/LPFileCollHeaderCell.m
@@ -0,0 +1,31 @@
+//
+//  LPFileCollHeaderCell.m
+//  LynxPainter
+//
+//  Created by DmitriyShamaev on 11/14/12.
+//  Copyright (c) 2012 DmitriyShamaev. All rights reserved.
+//
+
+#import "LPFileCollHeaderCell.h"
+
+@implementation LPFileCollHeaderCell
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
