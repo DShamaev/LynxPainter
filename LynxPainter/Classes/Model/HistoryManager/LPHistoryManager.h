526e96982c4d209f87de0f65b1f68def0bf5dd48
Dmitriy Shamaev <Dmitriy.Shamaev@dataart.com>
2012-11-19 00:38:14 +0400

    -history manager init

---
+++ LynxPainter/Classes/Model/HistoryManager/LPHistoryManager.h
@@ -0,0 +1,13 @@
+//
+//  LPHistoryManager.h
+//  LynxPainter
+//
+//  Created by DmitriyShamaev on 11/18/12.
+//  Copyright (c) 2012 DmitriyShamaev. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LPHistoryManager : NSObject
+
+@end
---
+++ LynxPainter/Classes/Model/HistoryManager/LPHistoryManager.m
@@ -0,0 +1,13 @@
+//
+//  LPHistoryManager.m
+//  LynxPainter
+//
+//  Created by DmitriyShamaev on 11/18/12.
+//  Copyright (c) 2012 DmitriyShamaev. All rights reserved.
+//
+
+#import "LPHistoryManager.h"
+
+@implementation LPHistoryManager
+
+@end
