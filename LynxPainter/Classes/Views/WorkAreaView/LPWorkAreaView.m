4d9f9ca155a14d6e3f8769e56821dce9687b0e33
Dmitriy Shamaev <Dmitriy.Shamaev@dataart.com>
2012-11-26 17:19:09 +0400

    -repo files fix

--- LynxPainter/Classes/Views/WorkAreaView/LPWorkAreaView.m
+++ LynxPainter/Classes/Views/WorkAreaView/LPWorkAreaView.m
@@ -11,7 +11,6 @@
 #import "LPSmartLayer.h"
 #import "LPSmartLayerDelegate.h"
 #import "LPHistoryManager.h"
-#import "LPEraserDelegate.h"
 #import <QuartzCore/QuartzCore.h>
 
 @interface LPWorkAreaView (){
