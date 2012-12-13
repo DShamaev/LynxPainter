5f05f18e02c088baa062314dac54a7dc4b50d95b
Dmitriy Shamaev <Dmitriy.Shamaev@dataart.com>
2012-12-13 19:45:57 +0400

    -new cells (for redesign)

---
+++ LynxPainter/Classes/Cells/ToolCell/LPToolCollCell.h
@@ -0,0 +1,13 @@
+//
+//  LPToolCell.h
+//  LynxPainter
+//
+//  Created by DmitriyShamaev on 12/13/12.
+//  Copyright (c) 2012 DmitriyShamaev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LPToolCell : UITableViewCell
+
+@end
